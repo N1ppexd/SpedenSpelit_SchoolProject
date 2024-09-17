4f040383b2b5b61ef563ae17bbbb7b751cc62bc9
N1ppexd <niilohanni@gmail.com>
2024-09-17 16:14:22 +0300

    writeByte ja initializeDisplay funktiot aloitettu

--- Project/display.cpp
+++ Project/display.cpp
@@ -1,15 +1,42 @@
 #include "display.h"
 
 
+const int resetPin = 12;      //SRCLR
+const int clockPin = 11;      //SRCLK //SHCP
+const int latchPin = 10;      //RCLK  //STCP
+const int outEnablePin = 9;   //OE
+const int dataPin = 8;        //SER / dsp
+
 void initializeDisplay(void)
 {
-// See requirements for this function from display.h
+    pinMode(resetPin, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+    pinMode(latchPin, OUTPUT);
+    pinMode(outEnablePin, OUTPUT);
+    pinMode(dataPin, OUPUT);
+
+    digitalWrite(outEnablePin, LOW);
+    digitalWrite(resetPin, HIGH);
 }
 
 
 void writeByte(uint8_t bits,bool last)
 {
-// See requirements for this function from display.h
+    if(!last){ return; }
+
+    digitalWrite(latchPin, LOW);
+
+    for(int i = 0; i < 8; i++){
+        digitalWrite(clockPin, LOW);
+
+        //set either 1 or 0 based on the rightmost bit on the bits...
+        uint8_t shiftedBits = bits;
+        digitalWrite((shiftedBits >> i) & 1);
+
+        digitalWrite(clockPin, HIGH);
+    }
+
+    digitalWrite(latchPin, HIGH);
 }
 
 
@@ -22,4 +49,3 @@ void showResult(byte number)
 {
 // See requirements for this function from display.h
 }
-
