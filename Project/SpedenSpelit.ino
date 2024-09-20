a22e9eae777336b87a95f045dd26bd7425df0917
Muussinuijat <t3lihe00@students.oamk.fi>
2024-09-20 21:00:00 +0300

    tämmöstä

--- Project/SpedenSpelit.ino
+++ Project/SpedenSpelit.ino
@@ -7,6 +7,8 @@
 // loop() function and interrupt handlers
 volatile int buttonNumber = -1;           // for buttons interrupt handler
 volatile bool newTimerInterrupt = false;  // for timer interrupt handler
+volatile int interruptCount = 0;
+
 
 void setup()
 {
@@ -25,7 +27,7 @@ void loop()
     startTheGame();
   } 
      // check the game if 0<=buttonNumber<4
-  if (buttonNumber >= 0 & buttonNumber < 4) {
+  if (buttonNumber >= 0 && buttonNumber < 4) {
     checkGame(buttonNumber);
   }
      
@@ -45,10 +47,28 @@ void loop()
 
 void initializeTimer(void)
 {
+  int timerLimit = 6250;
+  noInterrupts(); // disable interrupts while setting up the timer
+  TCCR1A = 0; // clear timer1 control register A
+  TCCR1B = 0; // clear timer1 control register B
+  TCCR1B |= (1 << WGM12) | (1 << CS12);
+  OCR1A = timerLimit; // ten times a second.
+  TIMSK1 |= (1 << OCIE1A); 
+  interrupts(); // Re-enable
 	// see requirements for the function from SpedenSpelit.h
 }
+
 ISR(TIMER1_COMPA_vect)
 {
+newTimerInterrupt = true;
+interruptCount++;
+
+if (interruptCount >= 10) {
+  cli(); // clear interrupts = disable interrupts
+  OCR1A = OCR1A * 0.9; // Decreasing OCR1A by 10%
+  interruptCount = 0; // reset the interrupt count
+  sei(); // Re-enable interrupts
+  }
   /*
   Communicate to loop() that it's time to make new random number.
   Increase timer interrupt rate after 10 interrupts.
