6b4f0bf59d92313363a08becc8d43a99a9fcbc04
N1ppexd <niilohanni@gmail.com>
2024-09-29 18:40:51 +0300

    Paskaa

--- Project/SpedenSpelit/audio.h
+++ Project/SpedenSpelit/audio.h
@@ -4,10 +4,19 @@
 
 const byte buzzerPin = A0;
 
+/// @brief use playAudio to make sound effect shit
+/// @param  
 void playButtonPressAudio(void);
+/// @brief use playAudio to make sound effect shit
+/// @param  
 void playLoseAudio(void);
+/// @brief use playAudio to make sound effect shit
+/// @param  
 void playStartAudio(void);
 
+/// @brief Play audio
+/// @param frequency frequency
+/// @param timeInMilliseconds time
 void playAudio(int frequency, int timeInMilliseconds);
 
 #endif
